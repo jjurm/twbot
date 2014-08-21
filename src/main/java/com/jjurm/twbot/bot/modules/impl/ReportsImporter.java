89c2b1aeb4c8f31e00a6419fab909e33fe86078d
jjurm <micko.juraj64@gmail.com>
2014-08-21 12:03:51 +0200

    Updated ReportsImporter to fit TribalWars v8.25

--- src/main/java/com/jjurm/twbot/bot/modules/impl/ReportsImporter.java
+++ src/main/java/com/jjurm/twbot/bot/modules/impl/ReportsImporter.java
@@ -10,18 +10,19 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.commons.configuration.HierarchicalConfiguration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.joda.time.DateTime;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.w3c.dom.Element;
 
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.html.DomText;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.jjurm.twbot.bot.Bot;
 import com.jjurm.twbot.bot.InterruptedExecutionException;
@@ -31,7 +32,6 @@ import com.jjurm.twbot.bot.modules.AbstractModule;
 import com.jjurm.twbot.bot.modules.Module;
 import com.jjurm.twbot.bot.modules.ModuleFactory;
 import com.jjurm.twbot.control.Command;
-import com.jjurm.twbot.system.config.Config;
 import com.jjurm.twbot.tribalwars.TWEnvironment;
 import com.jjurm.twbot.tribalwars.Urls;
 
@@ -176,6 +176,7 @@ public class ReportsImporter extends AbstractModule {
 	void processReport(HtmlPage page, Connection con) throws SQLException {
 
 		List<String> resources = Arrays.asList(TWEnvironment.resources);
+		List<String> buildings = Arrays.asList(TWEnvironment.buildings);
 
 		String xpath1 = "//table[contains(@class,'vis')]//td[contains(@class,'nopad')]/table[contains(@class,'vis')][2]";
 		HtmlElement report = page.getFirstByXPath(xpath1);
@@ -198,144 +199,131 @@ public class ReportsImporter extends AbstractModule {
 		String query1 = "CALL update_unit_home_timestamp(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 		String query2 = "CALL update_unit_timestamp(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 		HtmlElement tbody = page.getFirstByXPath("//*[@id='attack_info_def_units']/tbody");
-		HtmlElement unit_out = page
-				.getFirstByXPath("//*[@id='attack_spy']/tbody/tr/td[@colspan='2']/table[contains(@class,'vis')]/tbody/tr[2]");
-		boolean out = unit_out != null;
-		try (PreparedStatement stmt_unit_home = con.prepareStatement(query1);
-				PreparedStatement stmt_unit = con.prepareStatement(query2)) {
-			stmt_unit_home.setInt(1, id);
-			stmt_unit_home.setTimestamp(2, new Timestamp(timestamp));
-			if (out) {
-				stmt_unit.setInt(1, id);
-				stmt_unit.setTimestamp(2, new Timestamp(timestamp));
-			}
-			for (int i = 0; i < TWEnvironment.units.length; i++) {
-				DomText start_count_el = tbody.getFirstByXPath(
-						"./tr[2]/td[contains(@class,'unit-item')][" + (i + 1) + "]/text()");
-				int start_count = Integer.parseInt(start_count_el.getTextContent().trim());
-				DomText loss_count_el = tbody.getFirstByXPath(
-						"./tr[3]/td[contains(@class,'unit-item')][" + (i + 1) + "]/text()");
-				int loss_count = Integer.parseInt(loss_count_el.getTextContent().trim());
-				stmt_unit_home.setInt(i + 3, start_count - loss_count);
+		if (tbody != null) {
+			HtmlElement unit_out = page
+					.getFirstByXPath("//*[@id='attack_spy_away']//table[contains(@class,'vis')]/tbody/tr[2]");
+			boolean out = unit_out != null;
+			try (PreparedStatement stmt_unit_home = con.prepareStatement(query1);
+					PreparedStatement stmt_unit = con.prepareStatement(query2)) {
+				stmt_unit_home.setInt(1, id);
+				stmt_unit_home.setTimestamp(2, new Timestamp(timestamp));
 				if (out) {
-					DomText out_count_el = unit_out.getFirstByXPath("./td[" + (i + 1)
-							+ "]/text()");
-					int out_count = Integer.parseInt(out_count_el.getTextContent().trim());
-					stmt_unit.setInt(i + 3, start_count - loss_count + out_count);
+					stmt_unit.setInt(1, id);
+					stmt_unit.setTimestamp(2, new Timestamp(timestamp));
 				}
+				for (int i = 0; i < TWEnvironment.units.length; i++) {
+					DomText start_count_el = tbody.getFirstByXPath(
+							"./tr[2]/td[contains(@class,'unit-item')][" + (i + 1) + "]/text()");
+					int start_count = Integer.parseInt(start_count_el.getTextContent().trim());
+					DomText loss_count_el = tbody.getFirstByXPath(
+							"./tr[3]/td[contains(@class,'unit-item')][" + (i + 1) + "]/text()");
+					int loss_count = Integer.parseInt(loss_count_el.getTextContent().trim());
+					stmt_unit_home.setInt(i + 3, start_count - loss_count);
+					if (out) {
+						DomText out_count_el = unit_out.getFirstByXPath("./td[" + (i + 1)
+								+ "]/text()");
+						int out_count = Integer.parseInt(out_count_el.getTextContent().trim());
+						stmt_unit.setInt(i + 3, start_count - loss_count + out_count);
+					}
+				}
+				stmt_unit_home.execute();
+				if (out) {
+					stmt_unit.execute();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
-			stmt_unit_home.execute();
-			if (out) {
-				stmt_unit.execute();
-			}
-		} catch (NullPointerException e) {
-			// ignore - no visible defender's units
-		} catch (SQLException e) {
-			e.printStackTrace();
 		}
 
-		spy_result: try { // ===== Spy result =====
-			HtmlElement attack_spy = page.getFirstByXPath("//*[@id='attack_spy']");
-			if (attack_spy == null)
-				break spy_result;
+		// ===== Spy results =====
+		resources: { // Resources
+			HtmlElement res = page
+					.getFirstByXPath("//table[@id='attack_spy_resources']/tbody/tr/td");
+			if (res == null)
+				break resources;
+			HtmlElement el;
+			DomText txtNode;
+			int[] resValues = new int[3];
 
-			resources: try { // Resources
-				HtmlElement res = attack_spy.getFirstByXPath("./tbody/tr[1]/td");
-				if (res == null)
-					break resources;
-				HtmlElement el;
-				DomText txtNode;
-				int[] resValues = new int[3];
-
-				List<String> ress = new ArrayList<String>();
-				for (String r : resources) {
-					el = res.getFirstByXPath("./span[contains(@class,'" + r + "')]");
-					if (el != null)
-						ress.add(r);
+			List<String> ress = new ArrayList<String>();
+			for (String r : resources) {
+				el = res.getFirstByXPath("./span[contains(@class,'" + r + "')]");
+				if (el != null)
+					ress.add(r);
+			}
+			for (String r : ress) {
+				txtNode = res.getFirstByXPath("./span[contains(@class,'" + r
+						+ "')]/following-sibling::text()[1]");
+				String t = txtNode.getTextContent().trim();
+				el = txtNode
+						.getFirstByXPath("./following-sibling::span[contains(@class,'grey') and preceding-sibling::span[1][contains(@class,'"
+								+ r + "')]]");
+				if (el != null) {
+					txtNode = el.getFirstByXPath("./following-sibling::text()[1]");
+					t += txtNode.getTextContent().trim();
 				}
-				for (String r : ress) {
-					txtNode = res.getFirstByXPath("./span[contains(@class,'" + r
-							+ "')]/following-sibling::text()[1]");
-					String t = txtNode.getTextContent().trim();
-					el = txtNode
-							.getFirstByXPath("./following-sibling::span[contains(@class,'grey') and preceding-sibling::span[1][contains(@class,'"
-									+ r + "')]]");
-					if (el != null) {
-						txtNode = el.getFirstByXPath("./following-sibling::text()[1]");
-						t += txtNode.getTextContent().trim();
-					}
-					resValues[resources.indexOf(r)] = Integer.parseInt(t);
-				}
-
-				String sql = "CALL update_res_timestamp(?, ?, ?, ?, ?)";
-				try (PreparedStatement stmt = con.prepareStatement(sql)) {
-					stmt.setInt(1, id);
-					stmt.setTimestamp(2, new Timestamp(timestamp));
-					for (int i = 0; i < 3; i++)
-						stmt.setInt(i + 3, resValues[i]);
-					stmt.execute();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			} catch (IndexOutOfBoundsException e) {
-				// ignore - no resources
+				resValues[resources.indexOf(r)] = Integer.parseInt(t);
 			}
 
-			building: try { // Buildings
-				HtmlElement buildings = attack_spy.getFirstByXPath("./tbody/tr[2]/td");
-				if (buildings == null)
-					break building;
+			String sql = "CALL update_res_timestamp(?, ?, ?, ?, ?)";
+			try (PreparedStatement stmt = con.prepareStatement(sql)) {
+				stmt.setInt(1, id);
+				stmt.setTimestamp(2, new Timestamp(timestamp));
+				for (int i = 0; i < 3; i++)
+					stmt.setInt(i + 3, resValues[i]);
+				stmt.execute();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 
-				String sql1 = "CALL update_building_timestamp(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-				String sql2 = "CALL update_prod_timestamp(?, ?, ?, ?, ?)";
-				try (PreparedStatement stmt = con.prepareStatement(sql1);
-						PreparedStatement stmt_prod = con.prepareStatement(sql2)) {
-					stmt.setInt(1, id);
-					stmt.setTimestamp(2, new Timestamp(timestamp));
-					stmt_prod.setInt(1, id);
-					stmt_prod.setTimestamp(2, new Timestamp(timestamp));
-					double[] production = new double[3];
-					Arrays.fill(production, bot.getTWEnvironment().getProduction(0));
+		building: { // Buildings + production + pop_total
+			HtmlInput buildings_input = page
+					.getFirstByXPath("//input[@id='attack_spy_building_data']");
+			if (buildings_input == null)
+				break building;
+			String json = buildings_input.getAttribute("value");
+			JSONArray data = new JSONArray(json);
 
-					for (int i = 0; i < TWEnvironment.buildings.length; i++) {
-						String b = TWEnvironment.buildings[i];
-						String buildingName = Config.buildingNames.get(b);
-						DomText building = buildings.getFirstByXPath("./text()[contains(., '"
-								+ buildingName + "')]");
-						int level = 0;
-						if (building != null) {
-							HtmlElement el = buildings.getFirstByXPath("./text()[contains(., '"
-									+ buildingName + "')]/following-sibling::b[1]");
-							String s = el.getTextContent().trim();
-							Pattern pattern = Pattern.compile(" ([0-9]+)\\)");
-							Matcher matcher = pattern.matcher(s);
-							matcher.find();
-							String strLevel = matcher.group(1);
-							level = Integer.parseInt(strLevel);
-						}
-						stmt.setInt(i + 3, level);
-						int index = resources.indexOf(b);
-						if (index != -1) {
-							production[index] = bot.getTWEnvironment().getProduction(level);
-						}
-					}
+			String sql1 = "CALL update_building_timestamp(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+			String sql2 = "CALL update_prod_timestamp(?, ?, ?, ?, ?)";
+			try (PreparedStatement stmt = con.prepareStatement(sql1);
+					PreparedStatement stmt_prod = con.prepareStatement(sql2)) {
+				stmt.setInt(1, id);
+				stmt.setTimestamp(2, new Timestamp(timestamp));
+				stmt_prod.setInt(1, id);
+				stmt_prod.setTimestamp(2, new Timestamp(timestamp));
+				int[] levels = new int[TWEnvironment.buildings.length];
 
-					for (int i = 0; i < 3; i++) {
-						stmt_prod.setDouble(i + 3, production[i]);
-					}
-
-					stmt.execute();
-					stmt_prod.execute();
-				} catch (SQLException e) {
-					e.printStackTrace();
+				for (int i = 0; i < data.length(); i++) {
+					JSONObject obj = data.getJSONObject(i);
+					String building = obj.getString("id");
+					String slevel = obj.getString("level");
+					int level = Integer.parseInt(slevel);
+					levels[buildings.indexOf(building)] = level;
 				}
 
-			} catch (IndexOutOfBoundsException e) {
-				// ignore - no buildings
+				for (int i = 0; i < TWEnvironment.buildings.length; i++) {
+					String b = TWEnvironment.buildings[i];
+					int level = levels[i];
+					stmt.setInt(i + 3, level);
+					int rindex = resources.indexOf(b);
+
+					// production
+					if (rindex != -1) {
+						double production = bot.getTWEnvironment().getProduction(level);
+						stmt_prod.setDouble(rindex + 3, production);
+					}
+
+					// pop_total
+				}
+
+				stmt.execute();
+				stmt_prod.execute();
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
 
-		} catch (IndexOutOfBoundsException e) {
-			// ignore - means no spy result available
 		}
 
 		con.commit();
